188833c583be518e0324503e7eb2c3d7500244cf
Nithish Deepala <119877777+Nithish9652@users.noreply.github.com>
2025-01-01 22:45:39 +0530

    Time: 0 ms (100%), Space: 8.8 MB (38.26%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     int maxScore(string s) {
-        int maxi = 0;
-        if(s=="00" || s=="11"){
-        return 1;
+        int ones = count(s.begin(), s.end(), '1');
+        
+        int ans = 0;
+        int zeros = 0;
+        for (int i = 0; i < s.size() - 1; i++) {
+            if (s[i] == '1') {
+                ones--;
+            } else {
+                zeros++;
+            }
+            
+            ans = max(ans, zeros + ones);
         }
-        for(int i=1;i<s.length();i++){
-        string s1 = s.substr(0,i);
-        int c = 0;
-        for(int i=0;i<s1.length();i++){
-        if(s1[i]=='0'){
-        c++;
-        }
-        }
-        int d = 0;
-        string s2 = s.substr(i);
-         for(int i=0;i<s2.length();i++){
-        if(s2[i]=='1'){
-        d++;
-        }
-        }
-        maxi = max(maxi,c+d);
-        }
-        return maxi;
+        
+        return ans;
     }
 };
