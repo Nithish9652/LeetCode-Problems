49e3146252bc0c82f67ef6fc0113b8cced5a0542
Nithish Deepala <119877777+Nithish9652@users.noreply.github.com>
2025-07-12 00:03:51 +0530

    Time: 195 ms (46.93%), Space: 120.1 MB (53.63%) - LeetHub

--- 2466-maximum-segment-sum-after-removals/2466-maximum-segment-sum-after-removals.cpp
+++ 2466-maximum-segment-sum-after-removals/2466-maximum-segment-sum-after-removals.cpp
@@ -1,68 +1,46 @@
-class Disjoint{
-    public:
-    vector<long long>parent,size,num;
-    Disjoint(int n){
-        parent.resize(n+1);
-        size.resize(n+1,1);
-        num.resize(n+1);
-        for(int i=0;i<=n;i++){
-            parent[i]=i;
-        }
-    }
-    int findUpar(int node){
-        if(node==parent[node]){
-            return node;
-        }
-        return parent[node]=findUpar(parent[node]);
-    }
-
-    void UnionBySize(int u,int v){
-        int ulp_u=findUpar(u);
-        int ulp_v=findUpar(v);
-        if(ulp_u==ulp_v)return;
-        if(size[ulp_u]>size[ulp_v]){
-            parent[ulp_v]=ulp_u;
-            size[ulp_u]+=size[ulp_v];
-            num[ulp_u]+=num[ulp_v];
-        }
-        else{
-            parent[ulp_u]=ulp_v;
-            size[ulp_v]+=size[ulp_u];
-            num[ulp_v]+=num[ulp_u];
-        }
-    }
-};
 class Solution {
 public:
     vector<long long> maximumSegmentSum(vector<int>& nums, vector<int>& removeQueries) {
-        int n=nums.size();
-        int m=removeQueries.size();
-        vector<long long>ans;
-        Disjoint ds(n);
-        ans.push_back(0);
-        vector<int>present(n,0);
-        for(int i=0;i<n;i++){
-            ds.num[i]=nums[i];     //giving oject num the same value of nums
-        }
-        long long maxa=0;
-        for(int i=m-1;i>=0;i--){
-            int cur_ind=removeQueries[i];
-            present[cur_ind]=1;
-            if(cur_ind-1>=0 && present[cur_ind-1]==1){
-                ds.UnionBySize(cur_ind,cur_ind-1);              //if left is present then merge it
+        int n = nums.size();
+        vector<long long> res(n);
+        vector<long long> segSum(n);
+        vector<int> parent(n);
+        vector<bool> active(n, false);
+        iota(parent.begin(), parent.end(), 0);
+        long long currMax = 0;
+
+        function<int(int)> find = [&](int x) {
+            if (parent[x] != x)
+                parent[x] = find(parent[x]);
+            return parent[x];
+        };
+
+        auto unite = [&](int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX != rootY) {
+                parent[rootY] = rootX;
+                segSum[rootX] += segSum[rootY];
             }
-            if(cur_ind+1<n && present[cur_ind+1]==1){
-                ds.UnionBySize(cur_ind,cur_ind+1);              //if right is present then merge it also with current index
+        };
+
+        for (int i = n - 1; i >= 0; --i) {
+            res[i] = currMax;
+            int idx = removeQueries[i];
+            active[idx] = true;
+            segSum[idx] = nums[idx];
+
+            if (idx > 0 && active[idx - 1]) {
+                unite(idx, idx - 1);
             }
-            maxa=max(maxa,ds.num[ds.findUpar(cur_ind)]);        //take maximum at each merge
-            ans.push_back(maxa);
+
+            if (idx + 1 < n && active[idx + 1]) {
+                unite(idx, idx + 1);
+            }
+
+            currMax = max(currMax, segSum[find(idx)]);
         }
-        // for(auto it:ans){
-        //     cout<<it<<" ";
-        // }
-        ans.pop_back();                     //at last we get total sum value also we not need this so just removed
-        reverse(ans.begin(),ans.end());     
-        return ans;
-        
+
+        return res;
     }
 };
