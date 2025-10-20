4cd9c4028481b237c7c1243647253efa11a6fa8e
Nithish Deepala <119877777+Nithish9652@users.noreply.github.com>
2025-10-20 12:48:04 +0530

    Time: 0 ms (100%), Space: 17.4 MB (95.7%) - LeetHub

---
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& arr) {
+        int ans = 0;
+        for(int i=0;i<arr.size();i++){
+          if(arr[i]=="--X" || arr[i]=="X--"){
+            ans--;
+          }
+          else{
+            ans++;
+          }
+        }
+        return ans;
+    }
+};
